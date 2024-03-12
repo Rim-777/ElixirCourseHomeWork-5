7b1cd76573f23a0b5a28a85d7c039e28038c934c
rim-777 <rails.moss@gmail.com>
2024-03-12 13:32:41 +0000

    update example 2 with genserver

--- lib/phoenix_app/services/users/create_service.ex
+++ lib/phoenix_app/services/users/create_service.ex
@@ -3,13 +3,56 @@ defmodule PhoenixApp.Services.Users.CreateService do
   Create Users.
   """
 
+  alias Ecto.Changeset
+
+  use GenServer
+
   import Ecto.Query, warn: false
   alias PhoenixApp.Repo
   alias PhoenixApp.User
 
-  def exec(attrs \\ %{}) do
-    %User{}
-    |> User.changeset(attrs)
-    |> Repo.insert()
+  @timeout 5000
+
+  def start_link() do
+    GenServer.start_link(__MODULE__, [])
+  end
+
+  def exec(pid, attrs \\ %{}) do
+    GenServer.cast(pid, {:create, attrs})
+  end
+
+  def state(pid) do
+    GenServer.call(pid, :get_state)
+  end
+
+  @impl GenServer
+  def init(_attrs) do
+    {:ok, {:init, %User{}}, @timeout}
+  end
+
+  @impl GenServer
+  def handle_call(:get_state, _from, state) do
+    {:reply, state, state, @timeout}
+  end
+
+  @impl GenServer
+  def handle_cast({:create, attrs}, _state) do
+    result =
+      %User{}
+      |> User.changeset(attrs)
+      |> Repo.insert()
+
+    new_state =
+      case result do
+        {:ok, user} -> {:ok, user}
+        {:error, %Changeset{valid?: false, errors: errors}} -> {:error, errors}
+      end
+
+    {:noreply, new_state, @timeout}
+  end
+
+  @impl GenServer
+  def handle_info(:timeout, state) do
+    {:stop, :normal, state}
   end
 end
--- test/pnoenix_app/services/users/create_service_test.exs
+++ test/pnoenix_app/services/users/create_service_test.exs
@@ -3,34 +3,41 @@ defmodule PhoenixApp.Services.Users.CreateServiceTest do
   alias PhoenixApp.User
   alias PhoenixApp.Services.Users.CreateService
   alias PhoenixApp.Repo
-  alias Ecto.Changeset
 
   setup do
-    Ecto.Adapters.SQL.Sandbox.checkout(Repo)
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(PhoenixApp.Repo)
+    Ecto.Adapters.SQL.Sandbox.mode(Repo, {:shared, self()})
   end
 
   describe "exes" do
     test "stores a valid user in DB" do
+      {:ok, pid} = CreateService.start_link()
+
       attrs = %{
         name: "Matroskin",
         email: "matroskin@prostokvashino.quest",
         bio: "unbelievable"
       }
 
-      {:ok, %User{} = created_user} = CreateService.exec(attrs)
-
+      :ok = CreateService.exec(pid, attrs)
+      {:ok, created_user} = CreateService.state(pid)
       stored_user = Repo.get(User, created_user.id)
 
       assert created_user == stored_user
     end
 
     test "returns errors" do
-      errors = [
+      {:ok, pid} = CreateService.start_link()
+      :ok = CreateService.exec(pid, %{})
+
+      expected_errors = [
         {:name, {"can't be blank", [validation: :required]}},
         {:email, {"can't be blank", [validation: :required]}}
       ]
 
-      assert {:error, %Changeset{valid?: false, errors: ^errors}} = CreateService.exec(%{})
+      {:error, errors} = CreateService.state(pid)
+
+      assert errors == expected_errors
     end
   end
 end
